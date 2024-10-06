b140bcfa063b5ac87aa50a53ce80b032ce554a25
marlotea <73681911+marlotea@users.noreply.github.com>
2024-10-05 23:09:49 -0700

    AV API minor fix

--- api/alpha_vantage_api.py
+++ api/alpha_vantage_api.py
@@ -1,7 +1,8 @@
 import requests
 
+# fetches the daily series stocks
 def fetch_stock_data(symbol, api_key):
     url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
     response = requests.get(url)
-    data = response.json
+    data = response.json()
     return data['Time Series (Daily)']
